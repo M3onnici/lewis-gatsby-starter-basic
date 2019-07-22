fd5e9ab5ab5ff9d8ee688ed1a48cb689fb87e220
Lewis Llobera <billobera@gmail.com>
2019-07-22 17:50:30 +0200

    edit text style

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
 
-import { Head, HeadingXL, Layout, TextBody } from '../components';
+import { Head, HeadingXL, Layout } from '../components';
+
+const TextHome = styled.p`
+  max-width: 28em;
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 10vh;
+
+  font-size: 22px;
+  line-height: 1.6;
+  color: var(--dark-color-light);
+
+  @media (max-width: 849px) {
+    font-size: 19px;
+  }
+`;
 
 const Index = () => (
   <Layout>
     <Head title={`Home`} />
     <HeadingXL>Lewis Gatsby Starter Basic</HeadingXL>
-    <TextBody>
+    <TextHome>
       This is a custom basic Gatsby starter template to start a website.
-    </TextBody>
+    </TextHome>
   </Layout>
 );
 
