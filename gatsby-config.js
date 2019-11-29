e3f38ab03fcb702d30a168759ba91f3a819ab3ee
Lewis Llobera <billobera@gmail.com>
2019-11-29 10:01:09 +0100

    Add Netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `Lewis Gatsby Starter Basic`,
     description: `A custom basic Gatsby starter template to start a website.`,
+    siteUrl: `https://lewis-gatsby-starter-basic.netlify.com`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
